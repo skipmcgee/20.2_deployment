4073851cc7d2c93e5e01cd655c75751e4f3fc505
Skip McGee <davidfranklinmcgee@gmail.com>
2020-11-12 17:09:33 +0200

    updating js

--- add-in1.js
+++ add-in1.js
@@ -1,7 +1,12 @@
 /* Add at least 3 dynamic components to your web page. 
 These can be based on user input, or other dynamic input data. 
 
-At least one dynamic component should be based on user input.
 */
 
+var today = new Date();
+var hourNow = today.getHours();
+var dayNow = today.getDay();
+var minNow = today.getMinutes();
+var timeNow = (dayNow + hourNow + minNow);
 
+document.write(‘<h3>’ + timeNow + ‘</h3>’);
--- add-in3.js
+++ add-in3.js
@@ -1,3 +1,11 @@
 /* Add at least 3 dynamic components to your web page. 
 These can be based on user input, or other dynamic input data. 
+
+At least one dynamic component should be based on user input.
 */
+
+var userPrompt = prompt("Are you a Looter? (y/n):")
+
+if (userPrompt == 'y') {
+    document.write('<img src="images/american-flag.jpg" width="100"/>');
+} else continue;
