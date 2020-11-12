57a8c1c86b334940473c0722bb85d3d5f9861eff
Skip McGee <davidfranklinmcgee@gmail.com>
2020-11-12 16:44:06 +0200

    adding js input files

---
+++ add-in1.js
@@ -0,0 +1,7 @@
+/* Add at least 3 dynamic components to your web page. 
+These can be based on user input, or other dynamic input data. 
+
+At least one dynamic component should be based on user input.
+*/
+
+
