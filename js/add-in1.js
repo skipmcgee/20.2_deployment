90ad5e4a0bb3a7e2db640a6ff5544d0a62b8a6a2
Skip McGee <70141823+skipmcgee@users.noreply.github.com>
2020-11-13 10:04:36 +0200

    Delete add-in1.js

--- js/add-in1.js
+++
@@ -1,8 +0,0 @@
-/* Add at least 3 dynamic components to your web page. These can be based on user input, or other dynamic input data. */
-
-var today = new Date();
-var hourNow = today.getHours();
-var secNow = today.getSeconds();
-var minNow = today.getMinutes();
-
-document.write('<h2>' + 'The time is: ' + hourNow + ':' + minNow + ':' + secNow + '</h2>')
