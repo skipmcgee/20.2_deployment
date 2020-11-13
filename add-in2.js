ffdb913f86c8c2206353b7d5450f0e599c330c54
Skip McGee <70141823+skipmcgee@users.noreply.github.com>
2020-11-13 10:05:09 +0200

    Delete add-in2.js

--- add-in2.js
+++
@@ -1,7 +0,0 @@
-/* Add at least 3 dynamic components to your web page. 
-These can be based on user input, or other dynamic input data. 
-
-At least one dynamic component should use a conditional statement to determine the output.
-*/
-
-
